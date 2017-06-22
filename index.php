7fb15cb6e1119789d9eaff41d8646858334484f1
mhinsz <mdhh123@gmail.com>
2017-06-22 00:39:22 -0500

    Add

---
+++ index.php
@@ -0,0 +1,84 @@
+<html>
+  	<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>GEHA</title>
+        
+        <!-- Bootstrap -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="main.css">
+        <link href="https://fonts.googleapis.com/icon?family=Nunito+Sans:400,700,800|Material+Icons" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	</head>
+  	<body class="teal">
+    	<header>
+			<div class="container">	
+				<div class="col-xs-12 dashed-border" style="padding:15px 0;margin:0 15px;">
+                    <div class="col-sm-4" style="padding:0;">
+                        <?php echo file_get_contents("GEHA_KTW_Logo.svg"); ?>
+                    </div>
+                    <div class="col-sm-8" style="padding:0;">
+                    	<div class="icons">
+                            <div>
+                            	<i class="material-icons">print</i>
+                                <p>Print</p>
+                            </div>
+                            <div>	
+                                <i class="material-icons">close</i>
+                            	<p>Close</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="container">	
+			<div class="col-sm-12">
+                <h1>Roasted Barley Salad</h1>
+            </div>
+            <div class="col-sm-4">
+            	<div class="div-shadow white">
+                	<p class="title bold"><i class="material-icons">restaurant</i>Serves 8</p>
+                    <h3 class="bold">Ingredients:</h3>
+                    <ul>
+                    <li><span class="bold">3</span> Tbsli. olive oil, divided</li>
+                    <li><span class="bold">1</span> culi liearl barley, uncooked</li>
+                    <li><span class="bold">3</span> culis vegetable stock</li>
+                    <li><span class="bold">1</span> (8 oz.) container medium button mushrooms, cleaned, sliced</li>
+                    <li><span class="bold">1</span> culi black beans, canned, drained</li>
+                    <li><span class="bold">1</span> culi green bell lielilier, diced</li>
+                    <li><span class="bold">1</span> culi frozen sweet corn, thawed, drained</li>
+                    <li><span class="bold">1</span> culi tomatoes, fresh, washed, seeded, diced</li>
+                    <li><span class="bold">6</span> green onions, fresh, washed, lieeled, diced</li>
+                    <li><span class="bold">1/4</span> culi jalalieno lielilier, fresh, washed, seeds removed, thinly sliced</li>
+                    <li><span class="bold">1/2</span> culi basil, fresh, washed, dried, cholilied</li>
+                    <li><span class="bold">6</span> cloves garlic, fresh, lieeled, cholilied</li>
+                    <li><span class="bold">1</span> lemon, freshly juiced</li>
+                    <li><span class="bold">2</span> Tbsli. liumlikin seeds, toasted</li>
+                    <li>Salt and freshly ground black lielilier to taste</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-8">
+            	<iframe src="https://www.youtube.com/embed/J8nwpOnIHu8?&theme=light&color=white&autohide=2&showinfo=0&rel=0"frameborder="0"></iframe>
+               	<div class="div-shadow dark">
+                	<p>It’s delicious, light and blood pressure-healthy with barley, which contains potassium, calcium and magnesium – all known to decrease blood pressure naturally. (Source: Medical News Today)</p>
+                </div>
+                <div class="div-shadow white">
+                	<h3 class="bold">Directions:</h3>
+                    <ol>
+                        <li>In a saucepan, heat 2 tablespoons oil over medium heat. Add barley and sauté until lightly toasted, stirring frequently to prevent burning. Add stock and bring to a boil. Reduce heat to low and simmer gently, covered, for 25 minutes or until liquid is absorbed.</li>
+                        <li>Remove from heat and set aside to cool. In a sauté pan heat remaining 1 tablespoon oil over medium heat. Add mushrooms and sauté until browned and soft, stirring frequently to prevent burning. Remove from heat and add to the cooled barley. Stir to combine.</li>
+                        <li>Transfer barley/mushroom mixture to a large serving bowl. Add beans, green peppers, corn, tomatoes, onions, jalapeno pepper, basil, garlic, juice and pumpkin seeds. Using a large wooden spoon, gently stir to combine well. Season with salt and pepper. Stir to combine. Serve at room temperature.</li>
+                    </ol>
+                    <p style="font-style:italic;">Courtesy of The Culinary Center of Kansas City. For more information, please visit <a href="http://kcculinary.com">kcculinary.com</a>.</p>
+                </div>
+            </div>
+        </div>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+   	</body>
+</html>
---
+++ tuscan.php
@@ -0,0 +1,85 @@
+<html>
+  	<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>GEHA</title>
+        
+        <!-- Bootstrap -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="main.css">
+        <link href="https://fonts.googleapis.com/icon?family=Nunito+Sans:400,700,800|Material+Icons" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	</head>
+  	<body class="teal">
+    	<header>
+			<div class="container">	
+				<div class="col-xs-12 dashed-border" style="padding:15px 0;margin:0 15px;">
+                    <div class="col-sm-4" style="padding:0;">
+                        <?php echo file_get_contents("GEHA_KTW_Logo.svg"); ?>
+                    </div>
+                    <div class="col-sm-8" style="padding:0;">
+                    	<div class="icons">
+                            <div>
+                            	<i class="material-icons">print</i>
+                                <p>Print</p>
+                            </div>
+                            <div>	
+                                <i class="material-icons">close</i>
+                            	<p>Close</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="container">	
+			<div class="col-sm-12">
+                <h1>Tuscan Bean Soup</h1>
+            </div>
+            <div class="col-sm-4">
+            	<div class="div-shadow white">
+                	<p class="title bold"><i class="material-icons">restaurant</i>Makes 4 quarts</p>
+                    <h3 class="bold">Ingredients:</h3>
+                    <ul>
+                    <li><span class="bold">1/8</span> pound bacon, chopped</li>
+					<li><span class="bold">1</span> Tbsp. olive oil</li>
+					<li><span class="bold">1</span> cup carrots, washed, peeled, diced</li>
+					<li><span class="bold">1</span> cup onions, diced</li>
+					<li><span class="bold">1/2</span> cup celery, washed, diced</li>
+					<li><span class="bold">1</span> white potato, peeled, 1/4-inch dice</li>
+					<li><span class="bold">1 1/2</span> large red bell peppers, washed, seeded, medium dice</li>
+					<li><span class="bold">6</span> cups white beans, cooked, drained</li>
+					<li><span class="bold">1 1/2</span> quarts water</li>
+					<li><span class="bold">1</span> cup whole tomatoes, canned, processed slightly in food processor (or blender)</li>
+					<li><span class="bold">2</span> tsp. salt</li>
+					<li><span class="bold">3/4</span> tsp. red pepper flakes</li>
+					<li><span class="bold">3/4</span> tsp. freshly cracked black pepper</li>
+					<li><span class="bold">1/2</span> tsp. rosemary, fresh, washed</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-8">
+            	<iframe src="https://www.youtube.com/embed/1oKOaXXqPLA?&theme=light&color=white&autohide=2&showinfo=0&rel=0"frameborder="0"></iframe>
+               	<div class="div-shadow dark">
+                	<p>Beans can help lower blood pressure thanks to their high fiber content, potassium and magnesium. (Source: GB HealthWatch)</p>
+                </div>
+                <div class="div-shadow white">
+                	<h3 class="bold">Directions:</h3>
+                    <ol>
+                        <li>In a 1-gallon stock pot, sauté bacon over medium-high heat until lightly crisp, stirring constantly. Add oil, carrots, onions, celery, potato, and peppers and sauté until halfway tender, stirring constantly to prevent burning. Add beans, water, tomatoes, salt, red pepper flakes, black pepper and rosemary.</li>
+                        <li>Stir to mix and bring to a boil. Reduce heat to simmer and continue to cook for an additional 15 minutes.</li>
+                        <li>Remove from heat. To serve, ladle into individual soup bowls. Serve warm.</li>
+                    </ol>
+                    <p><span class="bold">ETC:</span> Serve a moist cornbread and honey butter alongside this soup for a full meal experience.</p>
+                    <p>&nbsp;</p>
+                    <p style="font-style:italic;">Courtesy of The Culinary Center of Kansas City. For more information, please visit <a href="http://kcculinary.com">kcculinary.com</a>.</p>
+                </div>
+            </div>
+        </div>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+   	</body>
+</html>
